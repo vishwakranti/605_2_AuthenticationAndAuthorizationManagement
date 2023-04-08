fd41354fca7571c12572abbe6a2f393267a01cd1
Vishwakranti Suryawanshi/Musalgavkar <87359394+vishwakranti@users.noreply.github.com>
2023-04-08 19:52:16 +1200

    added logout page and backend functionality to logout

--- Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,30 @@ namespace BikeSparePartsShop.Areas.Identity.Pages.Account
 {
     public class LogoutModel : PageModel
     {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly ILogger<LoginModel> _logger;
+
+        public LogoutModel(SignInManager<IdentityUser> signInManager, ILogger<LoginModel> logger)
+        {
+            _signInManager = signInManager;
+            _logger = logger;
+        }
         public void OnGet()
         {
+        }
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            returnUrl ??= Url.Content("~/");
+
+            if(ModelState.IsValid)
+            {
+                await _signInManager.SignOutAsync();
+
+                return LocalRedirect(returnUrl);
+            }
+
+            return LocalRedirect(returnUrl);
         }
     }
 }
